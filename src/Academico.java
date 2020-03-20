15addcbb66260f73d927ff51c47d136080484024
benjamindpb <benjamin.delpino@ug.uchile.cl>
2020-03-20 13:50:47 -0300

    primer commit: creo que esta todo bien, todo correcto

--- src/Academico.java
+++ src/Academico.java
@@ -1,2 +1,17 @@
-public class Academico {
+public class Academico extends Persona{
+    protected String paper;
+
+    public Academico(String nombre, String ocupacion, int edad, String paper) {
+        super(nombre, ocupacion, edad);
+        this.paper = paper;
+    }
+    public void dormir(){
+        System.out.println("ZzZzZzZ");
+    }
+    public void usarZoom(){
+        System.out.println("Hola alumnos aprendi a usar Zoom, miren mi gatito :3");
+    }
+    public String publicarPaper(){
+        return paper;
+    }
 }
--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,2 +1,16 @@
-public class Estudiante {
+public class Estudiante extends Persona {
+
+    public Estudiante(String nombre, String ocupacion, int edad) {
+        super(nombre, ocupacion, edad);
+    }
+
+    public void dormir(){
+        System.out.println("ZzZzZz");
+    }
+    public void verAnime(){
+        System.out.println("Death Note");
+    }
+    public void verNetflix(){
+        System.out.printf("Narcos");
+    }
 }
